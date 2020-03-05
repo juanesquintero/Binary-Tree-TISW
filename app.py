aa429ba0d107501523508d042086358acb0ddd83
unknown <juaneschrome@gmail.com>
2020-03-04 21:30:41 -0500

    Done!

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #hello
-from binarytree import Node, build
+from binarytree import Node, bst, build
 import numpy as np 
 
 n1 = int(input('Ingres el tamaño de la lista 1:  '))
@@ -8,5 +8,16 @@ n2 = int(input('Ingres el tamaño de la lista 2:  '))
 l1 = np.random.randint(low=1,high=100, size=n1)
 l2 = np.random.randint(low=1,high=100, size=n2)
 
-tree = build(np.sort(l1))
+print(l1,l2)
+
+lista = np.concatenate((l1,l2), axis=None)
+
+print(lista)
+
+tree = build(np.sort(l1)[::-1])
+tree2 = build(np.sort(l2)[::-1])
+
+tree3 = build(np.sort(lista)[::-1])
 print(tree) 
+print(tree2) 
+print(tree3) 
