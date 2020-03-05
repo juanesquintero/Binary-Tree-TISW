829d51e93498475d5a1f9f84930359e2d1bb74ba
unknown <juaneschrome@gmail.com>
2020-03-04 21:40:29 -0500

    Correction on prints

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ n2 = int(input('Ingres el tamaño de la lista 2:  '))
 l1 = np.random.randint(low=1,high=100, size=n1)
 l2 = np.random.randint(low=1,high=100, size=n2)
 
-print(l1,l2)
-
 lista = np.concatenate((l1,l2), axis=None)
 
-print(lista)
 
 tree = build(np.sort(l1)[::-1])
 tree2 = build(np.sort(l2)[::-1])
 
 tree3 = build(np.sort(lista)[::-1])
-print(tree) 
-print(tree2) 
-print(tree3) 
+
+print('lista 1: ', l1, 'lista 2: ', l2, 'Arbol 1: ', tree, 'Arbol 1: ', tree2, 'Lista Unificada: ',lista,'Arbol Unificado: ', tree3 )
+
