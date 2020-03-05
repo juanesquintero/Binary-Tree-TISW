05ddc791bae5da9e8850b3a1d58ccfbe48271927
unknown <juaneschrome@gmail.com>
2020-03-04 21:07:43 -0500

    Define lists

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 #hello
+from binarytree import Node, build
+import numpy as np 
+
+n1 = int(input('Ingres el tamaño de la lista 1:  '))
+n2 = int(input('Ingres el tamaño de la lista 2:  '))
+
+l1 = np.random.randint(low=1,high=100, size=n1)
+l2 = np.random.randint(low=1,high=100, size=n2)
+
+tree = build(np.sort(l1))
+print(tree) 
